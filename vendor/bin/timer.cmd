933e76df3cc11ba2c1396a59bdaadc67aa92e979
aureliony <39163684+aureliony@users.noreply.github.com>
2024-08-19 12:08:48 +0800

    Add more speedups

--- vendor/bin/timer.cmd
+++ vendor/bin/timer.cmd
@@ -1,22 +1,22 @@
 @echo off
 
-set start=%~1
-set end=%~2
+setlocal EnableDelayedExpansion
 
-set options="tokens=1-4 delims=:.,"
-for /f %options% %%a in ("%start%") do set start_h=%%a&set /a start_m=100%%b %% 100&set /a start_s=100%%c %% 100&set /a start_ms=100%%d %% 100
-for /f %options% %%a in ("%end%") do set end_h=%%a&set /a end_m=100%%b %% 100&set /a end_s=100%%c %% 100&set /a end_ms=100%%d %% 100
+for /f "tokens=1-4 delims=:., " %%a in ("%~1") do (
+    set /a start_h=%%a, start_m=1%%b %% 100, start_s=1%%c %% 100, start_ms=1%%d %% 100
+)
+for /f "tokens=1-4 delims=:., " %%a in ("%~2") do (
+    set /a end_h=%%a, end_m=1%%b %% 100, end_s=1%%c %% 100, end_ms=1%%d %% 100
+)
 
-set /a hours=%end_h%-%start_h%
-set /a mins=%end_m%-%start_m%
-set /a secs=%end_s%-%start_s%
-set /a ms=%end_ms%-%start_ms%
-if %ms% lss 0 set /a secs = %secs% - 1 & set /a ms = 100%ms%
-if %secs% lss 0 set /a mins = %mins% - 1 & set /a secs = 60%secs%
-if %mins% lss 0 set /a hours = %hours% - 1 & set /a mins = 60%mins%
-if %hours% lss 0 set /a hours = 24%hours%
-if 1%ms% lss 100 set ms=0%ms%
+:: Calculate differences
+set /a ms=end_ms-start_ms, secs=end_s-start_s, mins=end_m-start_m, hours=end_h-start_h
 
-:: Mission accomplished
-set /a totalsecs = %hours%*3600 + %mins%*60 + %secs%
-echo Elapsed Time: %hours%:%mins%:%secs%.%ms% (%totalsecs%.%ms%s total)
+:: Calculate total elapsed time in seconds
+set /a totalsecs = hours*3600 + mins*60 + secs
+
+:: Format milliseconds
+if %ms% lss 10 set ms=0%ms%
+
+:: Output result
+echo %totalsecs%.%ms%s
